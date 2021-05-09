ae891a15c8b2a3c2cd795ce864f226d5106624ed
Omkar A. Katta <omkarakatta@gmail.com>
2021-05-09 11:19:34 -0400

    Create get_initial_beta_D [gridsearch]

---
+++ R/gridsearch-aux-fcns.R
@@ -0,0 +1,32 @@
+### Meta -------------------------
+###
+### Title: Auxiliary Functions for Grid Search Procedure
+###
+### Description: Grid search can be decomposed into a series of steps:
+### 1. Create a grid
+### 2. Evaluate objective on a grid
+### 3. Find coordinate(s) with minimum objective
+### 4. Optionally create a new grid centered on the coordinate(s) from the
+###   previous step and repeat steps 2-4.
+###
+### Author: Omkar A. Katta
+###
+
+### get_initial_beta_D -------------------------
+#' Obtain naive coefficient estimates for \code{beta_D}
+#'
+#' Naively regress \code{Y} on \code{D}, \code{Z}, and \code{X}, and return
+#' the coefficients on \code{D}.
+#'
+#' @param Y Outcome matrix (more precisely, a column vector)
+#' @param D Matrix of endogenous variables
+#' @param Z Matrix of instruments
+#' @param X Matrix of covariates (incl. column of 1's for the intercept)
+#' @param ... Arguments to be passed to \code{quantreg::rq()}
+#'
+#' @return A vector of coefficients on the endogenous variable
+get_initial_beta_D <- function(Y, D, Z, X, ...) {
+  qr <- quantreg::rq(Y ~ D + Z + X - 1, ...)
+  stats::coef(qr)[seq_len(ncol(D))]
+}
+
