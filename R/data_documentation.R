20c12230635d9c2edf40911e0195e3a4f83c6df1
Omkar A. Katta <omkarakatta@gmail.com>
2021-05-10 15:14:54 -0400

    Add documentation for education data sets

--- R/data_documentation.R
+++ R/data_documentation.R
@@ -9,3 +9,36 @@
 #'   \item{\code{errors}}{model error and shocks defining D, 500 by 4 matrix}
 #' }
 "synthex1"
+
+#' @title Education-relevant Data
+#' @description 24684 observations with known education status
+#' \describe{
+#'    \item{\code{Y_educ}}{outcome variable, column matrix}
+#'    \item{\code{X_educ}}{covariate matrix, 50-column matrix}
+#'    \item{\code{D_educ}}{endogeneous matrix, column matrix (emp)}
+#'    \item{\code{Z_educ}}{instrument matrix, column matrix (emp_pct)}
+#'    \item{\code{EducFE_educ}}{education fixed effects matrix, 2-column matrix (hsgrad, hsplus)}
+#'    \item{\code{DInt_educ}}{endogeneous x education matrix, 2-column matrix}
+#'    \item{\code{ZInt_educ}}{instrument x education matrix, 2-column matrix}
+#' }
+#' @name education_autor
+"Y_educ"
+
+#' @rdname education_autor
+"X_educ"
+
+#' @rdname education_autor
+"D_educ"
+
+#' @rdname education_autor
+"Z_educ"
+
+#' @rdname education_autor
+"EducFE_educ"
+
+#' @rdname education_autor
+"DInt_educ"
+
+#' @rdname education_autor
+"ZInt_educ"
+
