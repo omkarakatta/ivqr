eb7bdc92d040e93c12e9be368d90b0aa8ea42082
Omkar A. Katta <omkarakatta@gmail.com>
2021-10-11 20:50:39 -0500

    Comment out code for debugging

--- R/mcmc_subsampling.R
+++ R/mcmc_subsampling.R
@@ -270,18 +270,18 @@ foc_membership <- function(
   stopifnot(ncol(xi) == 1)
 
   # DEBUG: see each entry of sum in xi object
-  s_i <- vector("list", length(noth))
-  for (i in seq_len(nrow(summand))) {
-    s_i[[i]] <- summand[i, ] %*% solve(designh)
-  }
-  s <- do.call("rbind", s_i)
+  # s_i <- vector("list", length(noth))
+  # for (i in seq_len(nrow(summand))) {
+  #   s_i[[i]] <- summand[i, ] %*% solve(designh)
+  # }
+  # s <- do.call("rbind", s_i)
 
   # check if xi satisfies FOC inequality
   left <- matrix(-1 * tau %*% rep(1, p), ncol = 1)
   right <- matrix((1 - tau) %*% rep(1, p), ncol = 1)
   list(
     status = all((left <= xi) & (xi <= right)), # returns TRUE if both are true, else FALSE
-    s = s, # return entries of xi object
+    # s = s, # return entries of xi object
     xi = xi
   )
 }
@@ -569,8 +569,10 @@ first_approach <- function(Y, X, D, Z, Phi = linear_projection(D, X, Z), tau,
   ones <- matrix(1, nrow = 1, ncol = length(subsample_set))
   sum_across_subsample_set <- ones %*% s[subsample_set, , drop = FALSE]
   subsample_weights <- vector("double", subsample_size - length(h))
+
   # TODO: remove `alt_subsample_weights` after debugging
-  alt_subsample_weights <- vector("double", subsample_size - length(h))
+  # alt_subsample_weights <- vector("double", subsample_size - length(h))
+
   # we have length(h) observations in subsample; we need subsample_size - length(h) more
   for (j in seq_len(subsample_size - length(h))) {
     choices <- setdiff(seq_len(nrow(Y)), subsample_set)
@@ -583,6 +585,8 @@ first_approach <- function(Y, X, D, Z, Phi = linear_projection(D, X, Z), tau,
 
     # for each row, apply e^(-gamma * (l-norm^l))
     raw_weights <- apply(sum_remaining, 1, function(x){
+      # TODO: compute the l^th power of l-norm!!!
+      # TODO: separate the `l` for the power and the `l` for the norm
       exp(-gamma * sum(abs(x)^l))
       # TODO: fix this
       # exp(-gamma * sum(abs(x)^l)^(1/l))
@@ -590,24 +594,26 @@ first_approach <- function(Y, X, D, Z, Phi = linear_projection(D, X, Z), tau,
     total_weights <- sum(unlist(raw_weights))
     weights <- raw_weights / total_weights
 
-    alt_weights <- apply(sum_remaining, 1, function(x){
-      # - tau < x < 1 - tau => - tau - x < 0 & x - (1 - tau) < 0
-      # => max{...} = 0 => we satisfy FOC
-      # Put differently:
-      # x is a 1 by p vector;
-      # So, x - (t - tau) and (- tau - x) are both 1 by p vectors.
-      # If each entry of these 1 by p vectors are negative, we satisfy FOC conditions
-      # Fix: `max` must be used element-wise!!! # Q: ask GP if this is right
-      left <- - tau - x
-      right <- x - (1 - tau)
-      max_result <- vector("double", length(x))
-      for (entry in seq_along(left)) {
-        left_entry <- left[[entry]]
-        right_entry <- right[[entry]]
-        max_result[[entry]] <- max(left_entry, right_entry, 0)
-      }
-      exp(-gamma * sum(abs(max_result))^l)
-    })
+    # alt_weights <- apply(sum_remaining, 1, function(x){
+    #   # - tau < x < 1 - tau => - tau - x < 0 & x - (1 - tau) < 0
+    #   # => max{...} = 0 => we satisfy FOC
+    #   # Put differently:
+    #   # x is a 1 by p vector;
+    #   # So, x - (t - tau) and (- tau - x) are both 1 by p vectors.
+    #   # If each entry of these 1 by p vectors are negative, we satisfy FOC conditions
+    #   # Fix: `max` must be used element-wise!!! # Q: ask GP if this is right
+    #   left <- - tau - x
+    #   right <- x - (1 - tau)
+    #   max_result <- vector("double", length(x))
+    #   for (entry in seq_along(left)) {
+    #     left_entry <- left[[entry]]
+    #     right_entry <- right[[entry]]
+    #     max_result[[entry]] <- max(left_entry, right_entry, 0)
+    #   }
+    #   # TODO: compute the l^th power of l-norm!!!
+    #   # TODO: separate the `l` for the power and the `l` for the norm
+    #   exp(-gamma * sum(abs(max_result))^l)
+    # })
 
     # choose 1 element in a single vector of size `length(weights)` to be 1
     winner <- tryCatch({
@@ -627,7 +633,7 @@ first_approach <- function(Y, X, D, Z, Phi = linear_projection(D, X, Z), tau,
     }
     winner <- winner$answer
     subsample_weights[[j]] <- weights[winner]
-    alt_subsample_weights[[j]] <- alt_weights[winner]
+    # alt_subsample_weights[[j]] <- alt_weights[winner]
     new_observation <- choices[winner]
     # TODO: store new_observation in new vector; then append to subsample_set after for loop
     subsample_set <- c(subsample_set, new_observation)
@@ -641,9 +647,9 @@ first_approach <- function(Y, X, D, Z, Phi = linear_projection(D, X, Z), tau,
     prob = prob, # return unnormalized probability of creating subsample
     log_prob = log_prob, # return log of unnormalized probability of creating subsample
     subsample_set = subsample_set, # return set of indices to create subsample!
-    alt_subsample_weights = alt_subsample_weights, # return alternative weights
+    # alt_subsample_weights = alt_subsample_weights, # return alternative weights
     subsample_weights = subsample_weights, # return weights for each observation in subsample
-    xi = sum_across_subsample_set, # return xi object # TODO: double-check that this is indeed the xi object
-    s = s # return each entry of term of xi for all observations, not just those in the subsample
+    # s = s, # return each entry of term of xi for all observations, not just those in the subsample
+    xi = sum_across_subsample_set # return xi object # TODO: double-check that this is indeed the xi object
   )
 }
