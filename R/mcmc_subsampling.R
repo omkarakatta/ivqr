81973c606653200ee1d302d91e77369a360d0960
Omkar A. Katta <omkarakatta@gmail.com>
2021-10-13 08:59:27 -0500

    Create alternatives to first_approach to propose our initial subsample
    first_approach:
      - original first approach
      - scale of probabilities was terrible
      - the last observation was chosen with the knowledge of all other
        subsamples. but this weight is kind of what we want for the entire
        subsample.
    
    first_approach_v2:
      - draw the observations in the subsample like we do with the active
        basis
      - problem: the weights are very different from one another. so, it is
        difficult to find draws where we only sample observations at most
        once.
    
    first_approach_v3:
      - this idea was abandoned so I didn't write the code.
      - the idea was that we sample one at a time BUT we don't change the
        relative probabilities.
      - I forget why we decided not to do this.
    
    first_approach_v4:
      - similar to first_approach
      - instead of using exponential, we use reciprocal
      - this created nicer weights that we can work with

--- R/mcmc_subsampling.R
+++ R/mcmc_subsampling.R
@@ -660,6 +660,9 @@ first_approach <- function(Y, X, D, Z, Phi = linear_projection(D, X, Z), tau,
 
 
 # TODO: document
+# propose all at once
+# problem: weights are very different from one another
+# so we can't gaurantee draws where we sample each observation at most once
 first_approach_v2 <- function(Y, X, D, Z, Phi = linear_projection(D, X, Z), tau,
                               h, subsample_size,
                               beta_D_proposal = NULL, beta_X_proposal = NULL, 
@@ -693,7 +696,7 @@ first_approach_v2 <- function(Y, X, D, Z, Phi = linear_projection(D, X, Z), tau,
   s <- do.call(rbind, s_i)
 
   ones <- matrix(1, nrow = 1, ncol = length(subsample_set))
-  sum_across_subsample_set <- ones %*% s[subsample_set, , drop = FALSE]
+  sum_across_subsample_set <- ones %*% s[subsample_set, , drop = FALSE] # this should be 0
   subsample_weights <- vector("double", subsample_size - length(h))
 
   choices <- setdiff(seq_len(nrow(Y)), subsample_set)
@@ -705,18 +708,19 @@ first_approach_v2 <- function(Y, X, D, Z, Phi = linear_projection(D, X, Z), tau,
   ones <- matrix(1, nrow = n_choices, ncol = 1)
   sum_remaining <- kronecker(ones, sum_across_subsample_set) + s_remaining
   raw_weights <- apply(sum_remaining, 1, function(x) {
-    left <- - tau - x
-    right <- x - (1 - tau)
-    max_result <- vector("double", length(x))
-    for (entry in seq_along(left)) {
-      left_entry <- left[[entry]]
-      right_entry <- right[[entry]]
-      max_result[[entry]] <- max(left_entry, right_entry, 0)
-    }
-    tmp <- sum(abs(max_result)^l_norm) ^ (1 / l_norm)
-    exp(-gamma * tmp^l_power)
-    # tmp <- sum(abs(x)^l_norm) ^ (1 / l_norm)
+    # left <- - tau - x
+    # right <- x - (1 - tau)
+    # max_result <- vector("double", length(x))
+    # for (entry in seq_along(left)) {
+    #   left_entry <- left[[entry]]
+    #   right_entry <- right[[entry]]
+    #   max_result[[entry]] <- max(left_entry, right_entry, 0)
+    # }
+    # tmp <- sum(abs(max_result)^l_norm) ^ (1 / l_norm)
     # exp(-gamma * tmp^l_power)
+    tmp <- sum(abs(x)^l_norm) ^ (1 / l_norm)
+    exp(-gamma * tmp^l_power)
+    # 0.99 ^ (-gamma * tmp^l_power)
   })
 
   # FIX: what if raw_weights are 0? then total_weights = 0, so then we get 0 / 0!!!
@@ -760,7 +764,7 @@ first_approach_v2 <- function(Y, X, D, Z, Phi = linear_projection(D, X, Z), tau,
   winner <- winner$answer
   new_observations <- choices[winner]
   subsample_set <- c(subsample_set, new_observations)
-  sum_across_subsample_set <- sum_across_subsample_set + s[new_observations, , drop = FALSE]
+  sum_across_subsample_set <- sum_across_subsample_set + matrix(1, nrow = 1, ncol = length(new_observations)) %*% s[new_observations, , drop = FALSE]
 
   subsample_weights <- weights[winner]
   prob <- prod(subsample_weights)
@@ -773,6 +777,133 @@ first_approach_v2 <- function(Y, X, D, Z, Phi = linear_projection(D, X, Z), tau,
     log_prob = log_prob, # return log of unnormalized probability of creating subsample
     subsample_set = subsample_set, # return set of indices to create subsample!
     subsample_weights = subsample_weights, # return weights for each observation in subsample
+    xi = sum_across_subsample_set # return xi object # TODO: double-check that this is indeed the xi object
+  )
+}
+
+# TODO: document this
+# replace exp{...} and replace with  1 / {...} or -log(...)
+first_approach_v4 <- function(Y, X, D, Z, Phi = linear_projection(D, X, Z), tau,
+                           h, subsample_size,
+                           beta_D_proposal = NULL, beta_X_proposal = NULL, 
+                           gamma = 1, l_norm = 1, l_power = l_norm) {
+  n <- length(Y)
+  p_design <- length(h)
+  subsample_set <- h # store indices of subsample
+  if (is.null(beta_D_proposal) | is.null(beta_X_proposal)) {
+    coef <- h_to_beta(h, Y = Y, X = X, D = D, Phi = Phi)
+    if (is.null(beta_D_proposal)) {
+      beta_D_proposal <- coef$beta_D
+    }
+    if (is.null(beta_X_proposal)) {
+      beta_X_proposal <- coef$beta_X
+    }
+  }
+  Y_tilde <- Y - D %*% beta_D_proposal
+  design <- cbind(X, Phi)
+  designh_inv <- solve(design[h, , drop = FALSE])
+
+  s_i <- vector("list", length = n)
+  for (i in seq_len(n)) {
+    if (is.element(i, h)) {
+      s_i[[i]] <- 0 # if index i is in active basis, set xi to be 0
+    } else {
+      # NOTE: beta_Phi should be 0
+      const <- (tau - as.numeric(Y_tilde[i] - X[i, ] %*% beta_X_proposal < 0))
+      s_i[[i]] <- const * design[i, ] %*% designh_inv
+    }
+  }
+  s <- do.call(rbind, s_i)
+
+  ones <- matrix(1, nrow = 1, ncol = length(subsample_set))
+  sum_across_subsample_set <- ones %*% s[subsample_set, , drop = FALSE]
+  subsample_weights <- vector("double", subsample_size - length(h))
+
+  # TODO: remove `alt_subsample_weights` after debugging
+  # alt_subsample_weights <- vector("double", subsample_size - length(h))
+
+  # we have length(h) observations in subsample; we need subsample_size - length(h) more
+  for (j in seq_len(subsample_size - length(h))) {
+    choices <- setdiff(seq_len(nrow(Y)), subsample_set)
+    n_choices <- length(choices)
+    s_remaining <- s[choices, , drop = FALSE]
+
+    # each row is one observation that we can choose from;
+    # the value is the weight in the exponent
+    ones <- matrix(1, nrow = n_choices, ncol = 1)
+    sum_remaining <- kronecker(ones, sum_across_subsample_set) + s_remaining
+
+    # for each row, apply e^(-gamma * (l-norm^l))
+    raw_weights <- apply(sum_remaining, 1, function(x) {
+      # exp(-gamma * sum(abs(x)^l)) # TODO: this is the old version, delete this
+      tmp <- sum(abs(x)^l_norm) ^ (1 / l_norm)
+      gamma / tmp^l_power
+      # exp(-gamma * tmp^l_power)
+    })
+    # FIX: what if raw_weights are 0? then total_weights = 0, so then we get 0 / 0!!!
+    # We still raise an error in rmultinom.
+    # But if we were to try repeating this in the main MCMC, we would still the same weights...
+    # So, we need to repeat the mcmc_active_basis to get a new proposal of the active basis and coefficients?
+    total_weights <- sum(unlist(raw_weights))
+    weights <- raw_weights / total_weights
+
+    # alt_weights <- apply(sum_remaining, 1, function(x) {
+    #   # - tau < x < 1 - tau => - tau - x < 0 & x - (1 - tau) < 0
+    #   # => max{...} = 0 => we satisfy FOC
+    #   # Put differently:
+    #   # x is a 1 by p vector;
+    #   # So, x - (t - tau) and (- tau - x) are both 1 by p vectors.
+    #   # If each entry of these 1 by p vectors are negative, we satisfy FOC conditions
+    #   # Fix: `max` must be used element-wise!!! # Q: ask GP if this is right
+    #   left <- - tau - x
+    #   right <- x - (1 - tau)
+    #   max_result <- vector("double", length(x))
+    #   for (entry in seq_along(left)) {
+    #     left_entry <- left[[entry]]
+    #     right_entry <- right[[entry]]
+    #     max_result[[entry]] <- max(left_entry, right_entry, 0)
+    #   }
+    #   # TODO: compute the l^th power of l-norm!!!
+    #   # TODO: separate the `l` for the power and the `l` for the norm
+    #   exp(-gamma * sum(abs(max_result))^l)
+    # })
+
+    # choose 1 element in a single vector of size `length(weights)` to be 1
+    winner <- tryCatch({
+      list(
+        status = "OKAY",
+        answer = which(rmultinom(n = 1, size = 1, prob = weights) == 1)
+      )
+    }, error = function(e) {
+      list(
+        status = "ERROR",
+        status_message = e,
+        sum_remaining = sum_remaining,
+        problem_weights = weights # return problematic weights
+      )
+    })
+    if (winner$status == "ERROR") {
+      return(winner)
+    }
+    winner <- winner$answer
+    subsample_weights[[j]] <- weights[winner]
+    # alt_subsample_weights[[j]] <- alt_weights[winner]
+    new_observation <- choices[winner]
+    # TODO: store new_observation in new vector; then append to subsample_set after for loop
+    subsample_set <- c(subsample_set, new_observation)
+    sum_across_subsample_set <- sum_across_subsample_set + s[new_observation, , drop = FALSE]
+  }
+  prob <- prod(subsample_weights)
+  log_prob <- sum(log(subsample_weights))
+  list(
+    status = "OKAY",
+    status_message = "OKAY",
+    prob = prob, # return unnormalized probability of creating subsample
+    log_prob = log_prob, # return log of unnormalized probability of creating subsample
+    subsample_set = subsample_set, # return set of indices to create subsample!
+    # alt_subsample_weights = alt_subsample_weights, # return alternative weights
+    subsample_weights = subsample_weights, # return weights for each observation in subsample
+    # s = s, # return each entry of term of xi for all observations, not just those in the subsample
     xi = sum_across_subsample_set # return xi object # TODO: double-check that this is indeed the xi object
   )
 }
