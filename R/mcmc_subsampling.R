45f43678f09f357be8df99ac0d63e1f69e12bec5
Omkar A. Katta <omkarakatta@gmail.com>
2021-10-23 16:16:21 -0500

    Create `compute_xi_i`

--- R/mcmc_subsampling.R
+++ R/mcmc_subsampling.R
@@ -1396,3 +1396,53 @@ find_subsample_in_polytope <- function(
     omega_mod = omega_mod
   )
 }
+
+### compute_xi_i -------------------------
+# TODO: incorporate this function into first_approach* and
+# find_subsample_in_polytope
+#' @param h Active basis in terms of the data provided
+#' @param Y Dependent variable (vector of length n)
+#' @param X Exogenous variable (including constant vector) (n by p_X matrix)
+#' @param D Endogenous variable (n by p_D matrix)
+#' @param Z Instrumental variable (n by p_Z matrix)
+#' @param Phi Transformation of X and Z to be used in the program;
+#'  defaults to the linear projection of D on X and Z (matrix with n rows)
+#' @param tau Quantile (numeric)
+compute_xi_i <- function(h,
+                         Y, X, D, Z, Phi = linear_projection(D, X, Z),
+                         tau) {
+  n <- nrow(Y)
+  p <- length(h)
+
+  # get beta_X_proposal and beta_D_proposal
+  if (is.null(beta_D_proposal) | is.null(beta_X_proposal)) {
+    coef <- h_to_beta(h, Y = Y, X = X, D = D, Phi = Phi)
+    if (is.null(beta_D_proposal)) {
+      beta_D_proposal <- coef$beta_D
+    }
+    if (is.null(beta_X_proposal)) {
+      beta_X_proposal <- coef$beta_X
+    }
+  }
+
+  Y_tilde <- Y - D %*% beta_D_proposal
+  design <- cbind(X, Phi)
+  designh_inv <- solve(design[h, , drop = FALSE])
+
+  s_i <- vector("list", length = n)
+  for (i in seq_len(n)) {
+    if (is.element(i, h)) {
+      s_i[[i]] <- 0 # if index i is in active basis, set xi to be 0
+    } else {
+      # NOTE: beta_Phi should be 0
+      const <- (tau - as.numeric(Y_tilde[i] - X[i, ] %*% beta_X_proposal < 0))
+      s_i[[i]] <- const * design[i, ] %*% designh_inv
+    }
+  }
+  s <- t(do.call(rbind, s_i))
+  xi_mat <- s[, setdiff(seq_len(n), h)]
+  stopifnot(nrow(xi_mat) == p)
+  stopifnot(ncol(xi_mat) == n - p)
+
+  xi_mat
+}
