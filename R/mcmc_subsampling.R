d9cd5f00dc2c6ed07a17970af9b102177ae4f4c9
Omkar A. Katta <omkarakatta@gmail.com>
2021-10-23 17:53:06 -0500

    Create `ot`

--- R/mcmc_subsampling.R
+++ R/mcmc_subsampling.R
@@ -1482,3 +1482,65 @@ compute_xi_i <- function(h,
 
   xi_mat
 }
+
+### ot -------------------------
+#' Compute OT between identical uniform distributions
+#'
+#' Compute transport map between U(1,...,n-p) and U(1,...,n-p) where C(i, j) =
+#' norm difference of pre[, i] and post[, j] where i and j are between 1 and
+#' n-p
+ot <- function(pre, post, params = list(OutputFlag = 0)) {
+  n_minus_p <- ncol(pre)
+  stopifnot(n_minus_p == (post))
+
+  # prelims
+  num_decision_vars <- n_minus_p^2
+
+  # compute cost matrix
+  c_ij <- matrix(0, nrow = n_minus_p, ncol = n_minus_p)
+  for (col in seq_along(n_minus_p)) {
+    # get diagonals
+    c_ij[col, col] <- sum(abs(pre[, col] - post[, col]))
+    for (i in seq_along(n_minus_p - col - 1)) {
+      row <- i + col
+      # create a lower-triangular matrix with the cost
+      c_ij[i, j] <- sum(abs(pre[, row] - post[, col]))
+    }
+  }
+  # fill out the matrix
+  c_ij[upper.tri(c_ij)] <- c_ij[lower.tri(c_ij)]
+
+  # create constraints
+  const_pre <- vector("list", length = n_minus_p)
+  const_post <- vector("list", length = n_minus_p)
+  for (i in seq_along(n_minus_p)) {
+    zeros_left <- matrix(0, nrow = n_minus_p, ncol = i - 1)
+    ones <- matrix(1, nrow = num_minus_p, ncol = 1)
+    zeros_right <- matrix(0, nrow = n_minus_p, ncol = n_minus_p - i)
+    a_mat <- cbind(zeros_left, ones, zeros_right)
+    const_pre[[i]] <- c(a_mat)
+    const_post[[i]] <- c(t(a_mat))
+  }
+  a_mat <- do.call(rbind, list(const_i, const_j))
+  stopifnot(nrow(a_mat) = num_decision_vars)
+  stopifnot(ncol(a_mat) = n_minus_p * 2)
+
+  # create program
+  model <- list()
+  model$obj <- c(c_ij) # turn into vector (go down each column)
+  model$a_mat <- a_mat
+  model$sense <- rep("=", length = n_minus_p * 2)
+  model$rhs <- rep(1, length = n_minus_p * 2)
+  model$vtype <- rep("B", num_decision_vars)
+
+  sol <- gurobi(model, params)
+  status <- sol$status
+  map <- matrix(sol$x, nrow = n_minus_p, ncol = n_minus_p)
+
+  list(
+    model = model,
+    sol = sol,
+    status = status,
+    map = map
+  )
+}
