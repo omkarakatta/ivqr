202995ae2c3b37992e7748b09a9b9996460a3d1f
Omkar A. Katta <omkarakatta@gmail.com>
2022-02-02 10:15:52 -0600

    Refactor `compute_xi_i` -> `compute_foc_conditions`

--- R/aux-fcns.R
+++ R/aux-fcns.R
@@ -195,45 +195,18 @@ p_val_interpolation <- function(old_p_val,
   list(beta_border = beta_border, pi = pi)
 }
 
-### compute_xi_i -------------------------
+# compute_foc_conditions -------------------------------------------------------
 
-# TODO: incorporate this function into first_approach* and
-# find_subsample_in_polytope
-#' @param h Active basis in terms of the data provided
-#' @param Y Dependent variable (vector of length n)
-#' @param X Exogenous variable (including constant vector) (n by p_X matrix)
-#' @param D Endogenous variable (n by p_D matrix)
-#' @param Z Instrumental variable (n by p_Z matrix)
-#' @param Phi Transformation of X and Z to be used in the program;
-#'  defaults to the linear projection of D on X and Z (matrix with n rows)
-#' @param tau Quantile (numeric)
-#' @param beta_D_proposal Coefficients on the endogeneous variables (vector of
-#'  length p_D); if NULL, use \code{h_to_beta} function and the \code{h}
-#'  argument to determine \code{beta_D_proposal}
-#' @param beta_X_proposal Coefficients on the exogeneous variables (vector of
-#'  length p_D); if NULL, use \code{h_to_beta} function and the \code{h}
-#'
-#' @return Matrix of dimension p by (n-p) of xi_i's not including the i's in the active basis
-compute_xi_i <- function(h,
-                         Y, X, D, Z, Phi = linear_projection(D, X, Z),
-                         tau,
-                         beta_D_proposal = NULL,
-                         beta_X_proposal = NULL) {
-  n <- nrow(Y)
-  p <- length(h)
-
-  # get beta_X_proposal and beta_D_proposal
-  if (is.null(beta_D_proposal) | is.null(beta_X_proposal)) {
-    coef <- h_to_beta(h, Y = Y, X = X, D = D, Phi = Phi)
-    if (is.null(beta_D_proposal)) {
-      beta_D_proposal <- coef$beta_D
-    }
-    if (is.null(beta_X_proposal)) {
-      beta_X_proposal <- coef$beta_X
-    }
-  }
-
-  Y_tilde <- Y - D %*% beta_D_proposal
+#' @param h Indices in active basis
+#' @param Y,X,D,Phi Data
+#' @param tau Quantile
+compute_foc_conditions <- function(
+  h,
+  Y, X, D, Phi,
+  tau
+) {
+  coef_full <- h_to_beta(h, Y = Y, X = X, D = D, Phi = Phi)
+  Y_tilde <- Y - D %*% coef_full$beta_D
   design <- cbind(X, Phi)
   designh_inv <- solve(design[h, , drop = FALSE])
 
@@ -249,8 +222,8 @@ compute_xi_i <- function(h,
   }
   s <- t(do.call(rbind, s_i))
   xi_mat <- s[, setdiff(seq_len(n), h), drop = FALSE]
-  stopifnot(nrow(xi_mat) == p)
-  stopifnot(ncol(xi_mat) == n - p)
+  # stopifnot(nrow(xi_mat) == p)
+  # stopifnot(ncol(xi_mat) == n - p)
 
   xi_mat
 }
